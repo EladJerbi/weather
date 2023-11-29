51f5a8495b854b51e0c9ba3d4dae13924aa3f851
eladjerbi <eladxjerbi@gmail.com>
2023-11-29 21:58:51 +0200

    checking integration with mongodb

--- start.sh
+++ start.sh
@@ -7,7 +7,4 @@ export $(grep -v '^#' .env | xargs)
 # Load environment variables from /vault/secrets/config file
 export $(grep -v '^#' /vault/secrets/config | xargs)
 
-# Load environment variables from /vault/secrets/mongodb-creds file
-export $(grep -v '^#' /vault/secrets/mongodb-creds | xargs)
-
 exec gunicorn -w 3 -b :80 app:app
